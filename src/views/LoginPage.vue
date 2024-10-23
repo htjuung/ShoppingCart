700dd9a086c09a76110e83a221d786b297884294
Tanya <htjuung@gmail.com>
2024-10-23 17:08:42 +0800

    Store the login token in a cookie.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -38,7 +38,10 @@ export default {
       const api = `${process.env.VUE_APP_API}/admin/signin`
       this.$http.post(api, this.user)
         .then((res) => {
+          const { token, expired } = res.data
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)}`
           console.log(res)
+          console.log(token, expired)
         })
     }
   }
